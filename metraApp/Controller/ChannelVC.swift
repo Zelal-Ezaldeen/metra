c13992cc0e0c1df2dba8a5de2a3b23abb7612c43
Zelal-Ezaldeen <zelalezaldeen@gmail.com>
2018-11-25 12:16:13 +0300

    Add  ChatVC and ChaneelVC

---
+++ metraApp/Controller/ChannelVC.swift
@@ -0,0 +1,24 @@
+//
+//  ChannelVC.swift
+//  metraApp
+//
+//  Created by Zelal-Ezaldeen on 11/25/18.
+//  Copyright © 2018 Metra Company. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+        
+
+    }
+    
+
+  
+
+}
---
+++ metraApp/Controller/ChatVC.swift
@@ -0,0 +1,29 @@
+//
+//  ChatVC.swift
+//  metraApp
+//
+//  Created by Zelal-Ezaldeen on 11/25/18.
+//  Copyright © 2018 Metra Company. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+
+       
+    }
+    
+
+ 
+
+}
