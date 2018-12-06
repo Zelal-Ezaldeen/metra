893fb6817df4f839e58128565158ab497c8864e2
Zelal-Ezaldeen <zelalezaldeen@gmail.com>
2018-12-06 12:03:13 +0300

    Add Channel 95

---
+++ metraApp/Controller/AddChannelVC.swift
@@ -0,0 +1,41 @@
+//
+//  AddChannelVC.swift
+//  metraApp
+//
+//  Created by Zelal-Ezaldeen on 12/6/18.
+//  Copyright © 2018 Metra Company. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+// Outlets
+    
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var chanDesc: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+      
+    }
+
+
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    @IBAction func closeModelPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "اسم المشروع", attributes: [NSAttributedString.Key.foregroundColor : metraPurplePlaceholder])
+           chanDesc.attributedPlaceholder = NSAttributedString(string: "وصف المشروع", attributes: [NSAttributedString.Key.foregroundColor : metraPurplePlaceholder])
+    }
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- metraApp/Controller/ChannelVC.swift
+++ metraApp/Controller/ChannelVC.swift
@@ -43,6 +43,13 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
        
     }
     
+    @IBAction func addChannelPressed(_ sender: Any) {
+        
+        let addChannel = AddChannelVC()
+        addChannel.modalPresentationStyle = .custom
+        present(addChannel, animated: true, completion: nil)
+        
+    }
     @objc func userDataChange(_ notif: Notification) {
      setupUserInfo()
     }
