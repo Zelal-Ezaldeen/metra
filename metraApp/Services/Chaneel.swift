6df9c47623c5a83a779c79ee8f76bd51793cb686
Zelal-Ezaldeen <zelalezaldeen@gmail.com>
2018-12-06 09:32:03 +0300

    Add GettingChannel 93

---
+++ metraApp/Services/Chaneel.swift
@@ -0,0 +1,17 @@
+//
+//  Chaneel.swift
+//  metraApp
+//
+//  Created by Zelal-Ezaldeen on 12/6/18.
+//  Copyright © 2018 Metra Company. All rights reserved.
+//
+
+import Foundation
+
+struct Channel : Decodable {
+    public private(set) var _id: String!
+    public private(set) var name: String!
+    public private(set) var description: String!
+    public private(set) var __v: Int?
+
+}
