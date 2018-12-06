ef60eb0a8ceaf1b2df89de86d4baa7645837e739
Zelal-Ezaldeen <zelalezaldeen@gmail.com>
2018-12-06 11:19:02 +0300

    Channels Table View 94

--- metraApp/Services/MessageService.swift
+++ metraApp/Services/MessageService.swift
@@ -20,29 +20,28 @@ class MessageService {
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
             
             if response.result.error == nil {
-                guard let data = response.data else {return}
+                guard let data = response.data else { return }
+               if let json = try! JSON(data: data).array {
                 
-                do {
-                    self.channels = try JSONDecoder().decode([Channel].self, from: data)
-                } catch let error {
-                    debugPrint(error as Any)
-                }
-                print(self.channels)
-//                if let json = try! JSON(data: data).array {
-//                    for item in json {
-//                        let name = item["name"].stringValue
-//                        let channelDescription = item["description"].stringValue
-//                        let id = item["_id"].stringValue
-//                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
-//                        self.channels.append(channel)
-//                        completion(true)
-//                    }
-//                }
+                    for item in json {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["_id"].stringValue
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+                    print(self.channels[0].channelTitle)
+                    completion(true)
+            }
+                
+                
                 
             } else {
                 completion(false)
                 debugPrint(response.result.error as Any)
             }
         }
+        
     }
+    
 }
